4ef6b8dba7b071281f5e775f672288d2997c4913
qiao <qiao@qiaodeMacBook-Air.local>
2021-07-27 22:12:08 +0800

    Reflactor: the err and unit test

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,17 +3,36 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
+	t.Run("knowe word", func(t *testing.T) {
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertString(t, got, want)
+		assertString(t, got, want)
+	})
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unkown")
+		// want := "could not find the word you were looking for"
+		if err == nil {
+			t.Fatal("expect to get an error")
+
+		}
+		assertError(t, err, ErrorNotFound)
+	})
+
 }
 
 func assertString(t *testing.T, got, want string) {
 	t.Helper()
 
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got '%s' want '%s'", got, want)
 	}
